0913349dc12673b25fe4f4c69ca12fe89fc01555
sawarn <57719908+sawarn@users.noreply.github.com>
2020-12-12 22:39:54 +0530

    Update R_Tools.py

--- R_Tools.py
+++ R_Tools.py
@@ -20,7 +20,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/Tools', methods = ["POST"])
+@app.route('/Tools', methods = ["GET"])
 def products():
     #data = request.get_json(force = "true")
     with open("Tools.json") as f:
