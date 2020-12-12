0d734083f20f50707bc7df8413a8c313447be229
sawarn <57719908+sawarn@users.noreply.github.com>
2020-12-12 22:39:28 +0530

    Update R_Seed.py

--- R_Seed.py
+++ R_Seed.py
@@ -22,7 +22,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/Seeds', methods = ["POST"])
+@app.route('/Seeds', methods = ["GET"])
 def products():
     #data = request.get_json(force = "true")
     with open("Seed.json") as f:
